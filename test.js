71e0787329f115631a8145ed66035ff4c5d56908
KwekusKid <2601677@students.wits.ac.za>
2025-05-16 19:03:57 +0200

    Blogweek10and11

---
+++ test.js
@@ -0,0 +1,85 @@
+function calculateRectangleArea() {
+    let length = 10;
+    let width = 5;
+    let area = calculateRectangleArea(length, width);
+    console.log("The area of the rectangle is: " + area);
+    return length * width;
+  }
+
+
+
+  
+
+  function calculateSum (i1, i2)
+  {
+    let sum = i1 + i2; 
+    if(i1 === i2){
+        console.log(sum);
+    return int1  * 3;
+    
+  }
+  }
+
+ 
+
+  function getRandomSum(int1, int2){
+   
+     int1 = Math.floor(Math.random() * 51);
+    int2 = Math.floor(Math.random() * 51 );
+
+    let sum = int1 + int2;
+
+    if(sum === 50 || (int1 || int2 === 50)){
+        console.log( "return is true because:" + sum === 50); 
+ return true;
+    }
+    else{
+        return false;
+    }
+
+
+    }
+
+    function exercise4(){
+ let power;
+ let current;
+ let voltage;
+
+power = voltage * current;
+console.debug(power);
+return power;
+
+
+    }
+
+  
+        function lessthan(x){
+     if(x < 5){
+        return true;
+     }
+    }
+
+let number = Math.floor(Math.random() * 11);
+
+let x = lessthan(number);
+
+if(x){
+  let string = "The number is less than 5";
+  console.log(string);
+}
+
+else{
+    let string = "Number is greater or equal to five";
+    console.log(string);
+}
+
+    
+   
+
+
+  
+
+  
+
+
+
